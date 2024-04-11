73af147b16f716b13772e660ffdda036442abd7d
Chris Waters <48170993+cwatersaudio@users.noreply.github.com>
2024-04-11 16:24:15 -0400

    conditional rendering of menu items

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -25,18 +25,20 @@ const Menu = () => {
                 <div className={open ? "menu-container full-menu" : "menu-container"} id="toggle">
                     <a href="#" className="menu" onClick={toggleOpen}><i className={open ? "fa fa-times" : "fa fa-bars"} aria-hidden="true"></i></a>
                 </div>
+                {open && (
+                    <div className="open overlay" id="overlay">
+                        <nav>
+                            <ul>
+                                <li><a href="#">Home</a></li>
+                                <li><a href="#">About</a></li>
+                                <li><a href="#">Blog</a></li>
+                                <li><a href="#">Contact</a></li>
+                            </ul>
+                        </nav>
+                    </div>)}
             </div>
 
-            <div className={open ? "open overlay" : "overlay"} id="overlay">
-                <nav>
-                    <ul>
-                        <li><a href="#">Home</a></li>
-                        <li><a href="#">About</a></li>
-                        <li><a href="#">Blog</a></li>
-                        <li><a href="#">Contact</a></li>
-                    </ul>
-                </nav>
-            </div>
+
         </div >
     )
 }
