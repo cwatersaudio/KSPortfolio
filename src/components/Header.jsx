f998ec0c8c09085b30492c322b5a48431c97843e
Chris Waters <48170993+cwatersaudio@users.noreply.github.com>
2024-04-11 15:41:56 -0400

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Menu from './Menu'
+
+const Header = () => {
+  return (
+    <header>
+      <div className='logo'>
+        <img src="" alt="" />
+      </div>
+      <Menu />
+      <nav className="nav">
+
+      </nav>
+    </header>
+  )
+}
+
+export default Header
---
+++ src/components/Menu.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+
+const Menu = () => {
+    const [open, setOpen] = React.useState(false);
+
+    // const menu = document.querySelector('#toggle');
+    // const menuItems = document.querySelector('#overlay');
+    // const menuContainer = document.querySelector('.menu-container');
+    // const menuIcon = document.querySelector('i');
+
+    // menu.addEventListener('click', () => {
+    //     menuItems.classList.toggle('open');
+    //     menuContainer.classList.toggle('full-menu');
+    //     menuIcon.classList.toggle('fa-bars');
+    //     menuIcon.classList.toggle('fa-times');
+    // });
+
+    function toggleOpen() {
+        setOpen(!open);
+
+    }
+    return (
+        <div className='menu' >
+            <div class="container">
+                <div className={open ? "menu-container full-menu" : "menu-container"} id="toggle">
+                    <a href="#" className="menu" onClick={toggleOpen}><i className="fa fa-bars" aria-hidden="true"></i></a>
+                </div>
+            </div>
+
+            <div className={open ? "open overlay" : "overlay"} id="overlay">
+                <nav>
+                    <ul>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">About</a></li>
+                        <li><a href="#">Blog</a></li>
+                        <li><a href="#">Contact</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div >
+    )
+}
+
+export default Menu
